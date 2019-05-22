edd3ada817ccb94c9c0cadd91f5daedc46a95255
Gulyás Róbert <robert@1studio.hu>
2019-05-22 23:51:52 +0200

    build(string): import new features

--- src/string.js
+++ src/string.js
@@ -6,14 +6,30 @@
  */
 
 import capitalizeFirstLetter from './string/capitalizeFirstLetter';
+import copyToClipboard from './string/copyToClipboard';
+import decrypt from './string/decrypt';
+import encrypt from './string/encrypt';
 import slugify from './string/slugify';
 import toNumber from './string/toNumber';
+import wordWrap from './string/wordWrap';
 
-export { capitalizeFirstLetter, slugify, toNumber };
+export {
+  capitalizeFirstLetter,
+  copyToClipboard,
+  decrypt,
+  encrypt,
+  slugify,
+  toNumber,
+  wordWrap,
+};
 
 export default
 {
   capitalizeFirstLetter,
+  copyToClipboard,
+  decrypt,
+  encrypt,
   slugify,
   toNumber,
+  wordWrap,
 };
