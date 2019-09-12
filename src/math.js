8e00e61af40bcf6527fe874010a196ddc1f3c986
Gulyás Róbert <robert@1studio.hu>
2019-09-12 14:23:48 +0200

    fix(math): add gcd import

--- src/math.js
+++ src/math.js
@@ -8,12 +8,14 @@
 import clamp from './math/clamp';
 import nthroot from './math/nthroot';
 import simplify from './math/simplify';
+import gcd from './math/gcd';
 
-export { clamp, nthroot, simplify };
+export { clamp, nthroot, simplify, gcd };
 
 export default
 {
   clamp,
   nthroot,
   simplify,
+  gcd,
 };
