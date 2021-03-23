1be3938673cd3160502f48baeb52fa1367943812
Gulyás Róbert <robert@1studio.hu>
2021-03-23 15:13:03 +0100

    feat(math): netting

---
+++ src/math/netting.js
@@ -0,0 +1,20 @@
+// @flow
+
+/**
+ * Netting
+ *
+ * @since x.xx.xx
+ * @static
+ * @memberof math
+ * @param  {int} gross
+ * @param  {int} vat
+ * @return {int}       net
+ * @example
+ *
+ * netting(127, 27);
+ * // => 100
+ */
+const netting = (gross: number, vat: number): number =>
+  gross / ((parseInt(vat) + 100) / 100);
+
+export default netting;
